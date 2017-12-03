9601c4853e74d3838635328addab8c327ef6a524
Sfeer <sfeer@sina.com>
2017-12-03 15:53:49 +0800

    添加外景四周场景

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Scene from '@/views/Scene'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Scene from '../views/Scene';
+import NewScene from '../views/NewScene';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   routes: [
     {path: '/', redirect: '/s1'},
     {path: '/:station(s\\d+)', component: Scene},
-    {path: '/scene/:scene', component: Scene}
+    {path: '/scene/:scene', component: Scene},
+    {path: '/d4', component: NewScene}
   ]
 })
