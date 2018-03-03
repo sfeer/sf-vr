34470078f0e90435dfa9f4faa8480449f8d90d77
Sfeer <sfeer@sina.com>
2018-03-03 22:51:23 +0800

    还原webpack@3开发   webpack@4配置待完善

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueResource from 'vue-resource'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(Element, {size: 'small'});
 Vue.use(VueResource);
 
 Vue.config.productionTip = false;
